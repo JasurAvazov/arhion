0516e3f17668f0a3107c4c4b5e2fb80a18fb379e
SArkor <abilkosimovsarkor@gmail.com>
2023-10-10 22:57:23 +0500

    feat: add smooth scroll

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
 <script setup>
+import { onMounted } from "vue";
+
+import SmoothScroll from "smoothscroll-for-websites";
+
+onMounted(() => {
+  SmoothScroll({
+    animationTime: 500,
+    stepSize: 60,
+    keyboardSupport: true,
+    arrowScroll: 100,
+    touchpadSupport: true,
+  });
+});
 </script>
 
 <template>
